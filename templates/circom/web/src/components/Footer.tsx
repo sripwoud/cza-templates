fb430e8e71d160bc562644b37a557dacd45ff84a
sripwoud <me@sripwoud.xyz>
2025-02-24 10:43:36 +0100

    chore: fmt

--- templates/circom/web/src/components/Footer.tsx
+++ templates/circom/web/src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import { ExternalLink } from "c/ExternalLink";
+import { ExternalLink } from 'c/ExternalLink'
 
 export const Footer = () => (
-  <footer className="flex justify-center items-center py-4">
-    <ExternalLink href="https://github.com/sripwoud/cza-templates/tree/main/templates/circom">
+  <footer className='flex justify-center items-center py-4'>
+    <ExternalLink href='https://github.com/sripwoud/cza-templates/tree/main/templates/circom'>
       <img
-        alt="GitHb"
-        height="32"
-        width="32"
-        src="https://cdn.simpleicons.org/github/c800de"
+        alt='GitHb'
+        height='32'
+        width='32'
+        src='https://cdn.simpleicons.org/github/c800de'
       />
     </ExternalLink>
   </footer>
-);
+)
